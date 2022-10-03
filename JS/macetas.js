a694aeb53f090323ee8be903268ed253bc0ea484
Alejo Martinez <alejoomartinex11@gmail.com>
2022-10-03 18:25:45 -0300

    agregado fetch y otros arreglos

--- JS/macetas.js
+++ JS/macetas.js
@@ -1,9 +1,13 @@
 let litrosDeTierra = 0; 
 let cantidad = 0;
+let retornar = 0;
+
 const contenedorRespuesta = document.getElementById("contenedorRespuesta");
 let calcular = document.getElementById("calcular");
 let filaRespuesta = document.getElementById("filaRespuesta");
 let favoritos = document.getElementById("favoritos");
+const divMacetas = document.querySelector(".contenedorPregunta-tamaño")
+const select = document.createElement("select")
 
 const macetasFavoritas = [];
 
@@ -30,9 +34,6 @@ timer: 800,
 timerProgressBar: true,
 didOpen: () => {
   Swal.showLoading()
-  timerInterval = setInterval(() => {
-    b.textContent = Swal.getTimerLeft()
-  }, 100)
 },
 willClose: () => {
   clearInterval(timerInterval)
@@ -41,19 +42,18 @@ willClose: () => {
 }
 
 const macetasCalcular = ()=> {
-    litrosDeTierra = parseFloat(document.getElementById("ingresoLitros").value);
     cantidad = parseFloat(document.getElementById("ingresoCantidad").value);
-    let resultadoFinal = litrosDeTierra * 0.15
+    let resultadoFinal = retornar * 0.15
     resultadoFinal = resultadoFinal.toFixed(2) 
 
-    litrosDeTierra === 1 ? //Condicion 1
+    retornar === 1 ? //Condicion 1
      filaRespuesta.innerHTML += `<tr class = "filaRespuesta">
-    <td>${ingresoLitros.value} litro de tierra</td>
+    <td>${retornar} litro de tierra</td>
     <td>${resultadoFinal} litros de agua</td>
     <td>${ingresoCantidad.value * resultadoFinal} litros de agua</td> 
     </tr>` : // Condicion 2
     filaRespuesta.innerHTML += `<tr class = "filaRespuesta">
-    <td>${ingresoLitros.value} litros de tierra</td>
+    <td>${retornar} litros de tierra</td>
     <td>${resultadoFinal} litros de agua</td>
     <td>${resultadoFinal * ingresoCantidad.value} litros de agua</td>
     </tr>` 
@@ -65,19 +65,41 @@ calcular.addEventListener("click", (e)=> {
 } )
 
 const almacenarFavoritos = ()=> {
-    const maceta = new Maceta(ingresoLitros.value, ingresoCantidad.value);
+    const maceta = new Maceta(retornar, ingresoCantidad.value);
     macetasFavoritas.push(maceta)
     localStorage.setItem("macetas" ,JSON.stringify(macetasFavoritas))
-    Swal.fire({
-        title: '¡Agregado a favoritos! Visita la sección "favoritos" para verlas',
-        width: 300,
-        padding: '1em',
-        color: '#716add',
-        icon: "success",
-        background: "#C7D3F2"
-      })
+
+  btnSwal()
+}
+
+
+const pedirDatos = async () => {
+ const resp = await fetch("../data/data.json")
+ const data = await resp.json()
+ data.forEach(maceta => {
+  select.innerHTML += `<option value="${maceta.capacidad}">${maceta.capacidad} litros.`
+ }); 
+ divMacetas.append(select)
+}
+pedirDatos();
+
+select.addEventListener("change", (el) => {
+  retornar = el.target.value;
+  return retornar;
+})
+
+const btnSwal = ()=> {
+  Swal.fire({
+    title: '¡Agregado a favoritos! Visita la sección "favoritos" para verlas',
+    width: 300,
+    padding: '1em',
+    color: '#716add',
+    icon: "success",
+    background: "#C7D3F2"
+  })
 }
 
 favoritos.addEventListener("click", ()=> {
     almacenarFavoritos()
 })
+
